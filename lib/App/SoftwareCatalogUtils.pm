a382ff80ed19d6c7c5dcbd0d6ff662fb953cb35f
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-13 18:53:53 +0700

    Initial commit

---
+++ lib/App/SoftwareCatalogUtils.pm
@@ -0,0 +1,33 @@
+package App::SoftwareCatalogUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'CLI utilities related to Software::Catalog',
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Software::Catalog>
+
+L<App::swcat>
+
+=cut
